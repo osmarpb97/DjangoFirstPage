520dde93ef06db6ef0516aacf41cf67f73c565da
Osmar <osmarpb.97@gmail.com>
2018-03-25 14:28:43 -0600

    Pagina Interfaz

--- Joyeria/Apps/Ventas/admin.py
+++ Joyeria/Apps/Ventas/admin.py
@@ -3,5 +3,5 @@ from Joyeria.Apps.Ventas.models import *
 
 # Register your models here.
 admin.site.register(Producto)
-admin.site.register(Vendedor)
+admin.site.register(Comprador)
 admin.site.register(Venta)
--- Joyeria/Apps/Ventas/models.py
+++ Joyeria/Apps/Ventas/models.py
@@ -5,12 +5,14 @@ from django.db import models
 class Producto(models.Model):
     Nombre_Producto = models.CharField(max_length=30)
     Descripcion = models.CharField(max_length=100)
-    Precio_Venta = models.IntegerField(max_length=5)
-    Precio_Compra = models.IntegerField(max_length=5)
+    Precio_Venta = models.IntegerField()
+    Precio_Compra = models.IntegerField()
     Clave_Producto = models.CharField(max_length=6)
-    Existencias= models.IntegerField(max_length=5);
-    COLORES=(('O','Oro'),('P','Plata'),('B','Bronce'),('R','Rosa'),('T','Turco'))
+    Existencias= models.IntegerField()
+    COLORES=(('O','Oro'),('P','Plata'),('B','Cobre'),('R','Rosa'),('T','Turco'))
     color= models.CharField(max_length=1,choices=COLORES,default='O')
+    MATERIALES=(('A','Acero'),('O','Oro'),('P','Plata'),('C','Cobre'),('B','Bisuteria'))
+    material= models.CharField(max_length=1,choices=MATERIALES,default='A')
 
     def Nombre_Productos(self):
         cadena= "{0}, {1}"
@@ -19,10 +21,12 @@ class Producto(models.Model):
     def __str__(self):
         return  self.Nombre_Productos()
 
-class Vendedor(models.Model):
+class Comprador(models.Model):
     Nombre_Pila_Vendedor = models.CharField(max_length=10)
     Apellido_Paterno_Vendedor= models.CharField(max_length=10)
     Apellido_Materno_Vendedor= models.CharField(max_length=10)
+    Correo= models.EmailField()
+    Contraseña= models.CharField(max_length=20)
     Clave_Vendedor = models.AutoField(primary_key=True)
 
     def __str__(self):
@@ -31,7 +35,7 @@ class Vendedor(models.Model):
 
 
 class Venta(models.Model):
-    Vendedor=models.ForeignKey(Vendedor, null=False,blank=False,on_delete=models.CASCADE)
+    Comprador=models.ForeignKey(Comprador, null=False,blank=False,on_delete=models.CASCADE)
     Producto=models.ForeignKey(Producto, null=False,blank=False,on_delete=models.CASCADE)
     num_venta = models.AutoField(primary_key=True)
     fecha_venta = models.DateTimeField(auto_now=True)
@@ -39,4 +43,4 @@ class Venta(models.Model):
 
     def __str__(self):
         cadena = "{0} vendio {1} del producto {2} el {3}"
-        return cadena.format(self.Vendedor,self.cantidad,self.Producto,self.fecha_venta)
+        return cadena.format(self.Comprador,self.cantidad,self.Producto,self.fecha_venta)
